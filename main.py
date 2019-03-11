6487c804e689201f5bc307895b522fc92b2962ed
Edenskull <maxchamp@live.fr>
2019-03-11 21:50:50 +0100

    Main Function apport

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import plugins.bot as bot
+import plugins.db_sqlite as database
 
 from plugins.PyLogger import pylogger
 
@@ -10,7 +12,11 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 def main():
     username = sys.argv[1]
     password = sys.argv[2]
-    pass
+    instabot = bot.InstagramBotChecker(username, password)
+    instabot.login()
+    followers = instabot.check_followers()
+    filename = '{}.sqlite'.format(username)
+    database.check_if_in_database(filename, followers)
 
 
 if __name__ == '__main__':
